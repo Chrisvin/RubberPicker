86a3d86954384c28fa996c2cc5f3d55b3199f048
Jem <chrisvin1997@gmail.com>
2019-09-12 03:42:18 +0530

    Finish basic demo app

--- app/src/main/java/com/jem/MainActivity.kt
+++ app/src/main/java/com/jem/MainActivity.kt
@@ -2,8 +2,10 @@ package com.jem
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.RadioButton
 import android.widget.SeekBar
 import com.jem.rubberpicker.ElasticBehavior
+import com.jem.rubberpicker.RubberSeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +15,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         elasticBehavior.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                1 -> rubberSeekBar.setElasticBehavior(ElasticBehavior.CUBIC)
-                2 -> rubberSeekBar.setElasticBehavior(ElasticBehavior.LINEAR)
-                3 -> rubberSeekBar.setElasticBehavior(ElasticBehavior.RIGID)
+            when (findViewById<RadioButton>(checkedId).text) {
+                "Cubic" -> rubberSeekBar.setElasticBehavior(ElasticBehavior.CUBIC)
+                "Linear" -> rubberSeekBar.setElasticBehavior(ElasticBehavior.LINEAR)
+                "Rigid" -> rubberSeekBar.setElasticBehavior(ElasticBehavior.RIGID)
             }
         }
 
         stretchRange.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                stretchRangeValue.text = progress.toString()
                 rubberSeekBar.setStretchRange(progress.toFloat())
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -30,6 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         dampingRatio.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                dampingRatioValue.text = (progress.toFloat()/10).toString()
                 rubberSeekBar.setDampingRatio(progress.toFloat()/10)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -38,7 +42,9 @@ class MainActivity : AppCompatActivity() {
 
         stiffness.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                rubberSeekBar.setStiffness(progress.toFloat()*100)
+                val progressValue = if (progress!=0) progress * 50 else 1
+                stiffnessValue.text = (progressValue).toString()
+                rubberSeekBar.setStiffness(progressValue.toFloat())
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
@@ -46,7 +52,9 @@ class MainActivity : AppCompatActivity() {
 
         defaultThumbRadius.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                rubberSeekBar.setThumbRadius(progress.toFloat())
+                val progressValue = if (progress == 0) 1 else progress
+                defaultThumbRadiusValue.text = progressValue.toString()
+                rubberSeekBar.setThumbRadius(progressValue.toFloat())
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
@@ -54,6 +62,7 @@ class MainActivity : AppCompatActivity() {
 
         normalTrackWidth.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                normalTrackWidthValue.text = progress.toString()
                 rubberSeekBar.setNormalTrackWidth(progress.toFloat())
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -62,10 +71,19 @@ class MainActivity : AppCompatActivity() {
 
         highlightTrackWidth.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                highlightTrackWidthValue.text = progress.toString()
                 rubberSeekBar.setHighlightTrackWidth(progress.toFloat())
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        rubberSeekBar.setOnRubberSeekBarChangeListener(object: RubberSeekBar.OnRubberSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: RubberSeekBar, progress: Int, fromUser: Boolean) {
+                rubberSeekBarValue.text = progress.toString()
+            }
+            override fun onStartTrackingTouch(seekBar: RubberSeekBar) {}
+            override fun onStopTrackingTouch(seekBar: RubberSeekBar) {}
         })
     }
 }
